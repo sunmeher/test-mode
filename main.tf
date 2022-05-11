70e25e18d8291604f590004aa24796042dcfc619
sunmeher <45628173+sunmeher@users.noreply.github.com>
2022-05-11 16:52:53 +0530

    Created terraform module for nat-gateway main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "nat-gateway-example-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "nat-gateway-publicIP"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  zones               = ["1"]
+}
+
+resource "azurerm_public_ip_prefix" "example" {
+  name                = "nat-gateway-publicIPPrefix"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  prefix_length       = 30
+  zones               = ["1"]
+}
+
+
+
